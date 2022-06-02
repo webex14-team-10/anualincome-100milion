9c97fc451a5a76af9c057bc92ffbaa1f2f91e2b1
mitsuo34 <mitsuotajima80@gmail.com>
2022-06-02 18:26:28 +0900

    MyProfileルーターを作成し、動作確認した。

---
+++ src/views/MyProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="profile">
+    <h1>This is an my profile page</h1>
+  </div>
+  <div>
+    <div>
+      ニックネーム
+      <input type="text" v-model="userName" />
+    </div>
+    <div>
+      地域
+      <input type="text" v-model="userArea" />
+    </div>
+    <div>アイコン画像選択</div>
+    <div>
+      一言コメント
+      <input type="text" v-model="userCommnet" />
+    </div>
+    <div>
+      その他あなたに関する情報
+      <input type="text" />
+    </div>
+  </div>
+</template>
