58db63ca5ac6bb6599019cc680dc0ff9d07a9d80
mitsuo34 <mitsuotajima80@gmail.com>
2022-06-08 17:36:35 +0900

    RESASのapiを実装し自動で都道府県を取得し、都道府県で選択できるようにした。

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -9,8 +9,16 @@
     </div>
     <div>
       地域
-      <input type="text" v-model="userArea" />
+      <td>
+        <select id="shop_pref" name="input_pref" class="form-control input-lg">
+          <option value="">都道府県選択</option>
+          <option v-for="item in prefectures" v-bind:key="item.prefCode">
+            {{ item.prefName }}
+          </option>
+        </select>
+      </td>
     </div>
+
     <div>アイコン画像選択</div>
     <div>
       一言コメント
@@ -21,4 +29,30 @@
       <input type="text" />
     </div>
   </div>
+  //*開発上の確認ボタン
+  <button v-on:click="confirmBtn">oooo</button>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      //APIデータを受け取る配列を定義
+      prefectures: null,
+    };
+  },
+  methods: {
+    confirmBtn() {
+      console.log(this.prefectures);
+    },
+  },
+  mounted() {
+    axios
+      .get("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+        headers: { "X-API-KEY": "nMkLzHbdeIGmtqnCJROChntAAaQcbJWUU7sarj2V" },
+      })
+      .then((response) => (this.prefectures = response.data.result));
+  },
+};
+</script>
