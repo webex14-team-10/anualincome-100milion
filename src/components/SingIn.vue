bbbd2621f23c869223e1f4da3759be9ca1ca671d
funam <funamario071@gmail.com>
2022-06-20 21:35:35 +0900

    機能完成

--- src/components/SingIn.vue
+++ src/components/SingIn.vue
@@ -1,6 +1,29 @@
 <template>
-  <div id="login">
-    <button @click="googleSingIn">ログイン</button>
+  <div class="login">
+    <button @click="signIn">ログイン</button>
   </div>
 </template>
-<script></script>
+<script>
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+export default {
+  methods: {
+    signIn() {
+      const provider = new GoogleAuthProvider();
+      provider.addScope("");
+      const auth = getAuth();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          credential.accessToken;
+          result.user;
+          alert("ログイン成功");
+        })
+        .catch((error) => {
+          GoogleAuthProvider.credentialFromError(error);
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
--- src/components/SingOut.vue
+++ src/components/SingOut.vue
@@ -1,3 +1,20 @@
 <template>
-  <button>ログアウト</button>
+  <button @click="signOut">ログアウト</button>
 </template>
+<script>
+import { getAuth, signOut } from "firebase/auth";
+export default {
+  methods: {
+    signOut() {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          alert("ログアウト");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
